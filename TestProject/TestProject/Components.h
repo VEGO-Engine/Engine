0aa9cfdaeff0301fdaa1efc1cad1dbe6c0eb9f37
Benedikt Galbavy <if22b082@technikum-wien.at>
2024-01-09 17:29:14 +0100

    deleted missed folders

--- TestProject/TestProject/Components.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ECS.h"
-#include "TransformComponent.h"
-#include "SpriteComponent.h"
-#include "KeyboardController.h"
-#include "ColliderComponent.h"
-#include "TileComponent.h"
-
