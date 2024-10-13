68079d027960bbe84efdee3625be7a7edd9aa5bc
freezarite <markus.roth2001@yahoo.de>
2024-10-13 13:29:32 +0200

    made texture_cache map private

--- include/TextureManager.h
+++ include/TextureManager.h
@@ -17,11 +17,10 @@ class TextureManager
 			}
 		}
 
-		std::map<std::string, SDL_Texture*> texture_cache;
-
 		SDL_Texture* loadTexture(const char* fileName);
         static std::vector<SDL_Rect> splitSpriteSheet(SDL_Texture* spriteSheet, int width, int height, int spritesOnSheet);
 		static void draw(SDL_Renderer* renderer, SDL_Texture* texture, SDL_Rect src, SDL_Rect dest, bool flipped = false);
 	private:
 		Manager* manager;
+		std::map<std::string, SDL_Texture*> texture_cache;
 };
